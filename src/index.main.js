3205545621df7f640b39bf60d4b5a04cc29bf5f3
leoromero97 <leonardogerbacio@gmail.com>
2021-04-06 13:14:39 -0300

    Leonardo Romero | PE/03__onboarding--tutorial - Se creo la screen Tutorial para mobile y web - Se creo el componente ButtonTertiary - Se eliminaron dos colores globales - Se elimino el flujo de quiero donar del proyecto viejo - Se agrego la screen Tutorial en el ruteo de la app

--- src/index.main.js
+++ src/index.main.js
@@ -2,20 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import Cover from './screens/Onboarding/Cover';
+import Presentation from './screens/Onboarding/Presentation';
+import Tutorial from './screens/Tutorial';
 import MedicineSelector from './components/quiero-pedir/screens/medicine-selector';
 import Tablet from './components/quiero-pedir/screens/tablet';
 import FoundationsSelector from './components/quiero-pedir/screens/foundations-selector';
 import Scanner from './components/quiero-pedir/screens/scanner';
 import Camara from './components/quiero-pedir/screens/camara';
 import Confirmation from './components/quiero-pedir/screens/confirmation';
-
-import MedicineSelectorDonar from './components/quiero-donar/screens/medicine-selector';
-import TabletDonar from './components/quiero-donar/screens/tablet';
-import FoundationsSelectorDonar from './components/quiero-donar/screens/foundations-selector';
-import ScannerDonar from './components/quiero-donar/screens/scanner';
-import CamaraDonar from './components/quiero-donar/screens/camara';
-import ConfirmationDonar from './components/quiero-donar/screens/confirmation';
-import Presentation from './screens/Onboarding/Presentation';
 
 class Main extends React.Component {
   state = {
@@ -37,19 +31,13 @@ class Main extends React.Component {
         <Switch>
           <Route exact path="/" component={Cover} />
           <Route exact path="/presentation" component={Presentation} />
+          <Route exact path="/tutorial" component={Tutorial} />
           <Route exact path="/medicine-selector" render={(props) => <MedicineSelector{...props} medicamentos={this.medicamentos} />} />
           <Route exact path="/tablet" component={Tablet} />
           <Route exact path="/foundations-selector" render={(props) => <FoundationsSelector{...props} fundacion={this.fundacion} />} />
           <Route exact path="/scanner" component={Scanner} />
           <Route exact path="/camara" component={Camara} />
           <Route exact path="/confirmation" component={Confirmation} />
-
-          <Route exact path="/medicine-selector-donar" render={(props) => <MedicineSelectorDonar{...props} medicamentos={this.medicamentos} />} />
-          <Route exact path="/tablet-donar" component={TabletDonar} />
-          <Route exact path="/foundations-selector-donar" render={(props) => <FoundationsSelectorDonar{...props} fundacion={this.fundacion} />} />
-          <Route exact path="/scanner-donar" component={ScannerDonar} />
-          <Route exact path="/camara-donar" component={CamaraDonar} />
-          <Route exact path="/confirmation-donar" component={ConfirmationDonar} />
         </Switch>
       </main>
     );
--- src/screens/Onboarding/Presentation/index.js
+++ src/screens/Onboarding/Presentation/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import ButtonPrimary from '../../../components/Buttons/ButtonPrimary';
 import ButtonSecondary from '../../../components/Buttons/ButtonSecondary';
 import styles from './styles.module.scss';
@@ -8,7 +9,9 @@ function Presentation() {
     <div className={styles.container}>
       <h1 className={styles.title}>Te damos la bienvenida a <span className={styles.titleGreen}>Pre Entrevista</span></h1>
       <p className={styles.text}>Practica antes de tu primer entrevista de RRHH, o si ya tuviste una podés dejar tu experiencia y ayudar a alguien que la necesite</p>
-      <ButtonPrimary text="¡ Practicar !"/>
+      <Link to="/tutorial">
+        <ButtonPrimary text="¡ Practicar !"/>
+      </Link>
       <ButtonSecondary text="Cargar experiencia"/>
     </div>
   )
